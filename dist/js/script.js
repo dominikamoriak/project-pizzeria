0b3524086d7a04f13f6e85acc90478a4fefdce02
Dominika Moriak <dominika.moriak@gmail.com>
2023-02-17 14:17:57 +0100

    Edit function app.initMenu - js

--- dist/js/script.js
+++ dist/js/script.js
@@ -62,8 +62,12 @@
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
 
     initData: function(){
