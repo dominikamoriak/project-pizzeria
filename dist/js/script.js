58e5746383db72ca5657684fa563c2713b3ea077
Dominika Moriak <dominika.moriak@gmail.com>
2023-02-17 16:44:07 +0100

    Method renderInMenu, find menu container, add const menuContainer - js

--- dist/js/script.js
+++ dist/js/script.js
@@ -67,13 +67,16 @@
       const thisProduct = this;
 
       /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
 
       /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
       /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
 
       /* add element to menu */
-      
+
     }
   }
 
