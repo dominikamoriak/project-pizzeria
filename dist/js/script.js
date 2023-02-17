0524a691567f57c1c0632f6237e7295a733e9504
Dominika Moriak <dominika.moriak@gmail.com>
2023-02-17 13:49:51 +0100

    Add function initData in the const app - js

--- dist/js/script.js
+++ dist/js/script.js
@@ -66,6 +66,12 @@
       console.log('testProduct:', testProduct);
     },
 
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -74,6 +80,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
