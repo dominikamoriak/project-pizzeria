c03d6b1cb0e816dfccafc50bfed7618554bfbf95
Dominika Moriak <dominika.moriak@gmail.com>
2023-02-23 19:14:56 +0100

    Delete const clickableTrigger from initAccordion, fix problem - js

--- dist/js/script.js
+++ dist/js/script.js
@@ -109,12 +109,8 @@
     initAccordion(){
       const thisProduct = this;
 
-      /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
-      console.log(clickableTrigger);
-
       /* START: add event listener to clickable trigger on event click */
-      clickableTrigger.addEventListener('click', function(event) {
+      thisProduct.accordionTrigger.addEventListener('click', function(event) {
 
         /* prevent default action for event */
         event.preventDefault();
