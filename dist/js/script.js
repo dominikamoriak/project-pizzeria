ad6ae615c9aad67762986f0d1d78f43806bab4d3
Dominika Moriak <dominika.moriak@gmail.com>
2023-03-07 17:45:39 +0100

    Create new method setValue

--- dist/js/script.js
+++ dist/js/script.js
@@ -250,6 +250,17 @@
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
+
+    setValue(value){
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /* TODO: Add validation */
+
+      thisWidget.value = newValue;
+      thisWidget.input.value = thisWidget.value;
+    }
   }
 
   const app = {
