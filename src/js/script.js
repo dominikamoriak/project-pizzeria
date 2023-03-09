a5c171b7f40bf43afafd60d7d2f925c1140f1332
Dominika Moriak <dominika.moriak@gmail.com>
2023-03-09 16:17:50 +0100

    Change initActions

--- src/js/script.js
+++ src/js/script.js
@@ -270,9 +270,28 @@
     }
 
     initActions(){
-      
+      const thisWidget = this;
+
+      /* add 'change' as event listener and use the method setValue for thisWidget.input */
+      thisWidget.input.addEventListener('change', setValue(value));
+
+      /* prevent default action for this event */
+      thisWidget.value.preventDefault();
+
+      /* add 'click' as event listener and use the method setValue for thisWidget.linkDecrease */
+      thisWidget.linkDecrease.addEventListener('click', setValue(++thisWidget.value));
+
+      /* prevent default action for this event */
+      thisWidget.value.preventDefault();
+
+      /* add 'click' as event listener and use the method setValue for thisWidget.linkIncrease */
+      thisWidget.linkIncrease.addEventListener('click', setValue(--thisWidget.value));
+
     }
+    thisWidget.initActions();
+
   }
+  
 
   const app = {
     initMenu: function(){
