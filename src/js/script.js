ccbc2fe2b5f97a1d5e03caac92cc34985a02f3b4
Dominika Moriak <dominika.moriak@gmail.com>
2023-03-13 16:03:51 +0100

    Create a new class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -347,6 +347,23 @@
       thisWidget.element.dispatchEvent(event);
     }
   }
+
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+    }
+  }
   
 
   const app = {
