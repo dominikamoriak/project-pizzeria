59225bdddcded9bde72350da99ac64a178fed9cc
Dominika Moriak <dominika.moriak@gmail.com>
2023-03-24 16:54:17 +0100

    Add bubbles and new CustomEvent in method announce, class amountWidget

--- dist/js/script.js
+++ dist/js/script.js
@@ -417,7 +417,9 @@
     announce(){
       const thisWidget = this;
 
-      const event = new Event('updated');
+      const event = new CustomEvent('updated',{
+        bubbles: true
+      });
       thisWidget.element.dispatchEvent(event);
     }
   }
