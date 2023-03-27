2807de98f1a48ed5f6af7f2f07ed3c20a6101c44
Dominika Moriak <dominika.moriak@gmail.com>
2023-03-28 00:13:45 +0200

    Fix method initData, const app

--- dist/js/script.js
+++ dist/js/script.js
@@ -674,7 +674,7 @@
           console.log('parsedResponse', parsedResponse);
 
           // save parsedResponse as thisApp.data.products //
-          parsedResponse = thisApp.data.products;
+          thisApp.data.products = parsedResponse;
 
           // execute initMenu method //
           thisApp.initMenu();
