d8fc2cc1940e7a2280bcbaa1b39444212d7b9a4e
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-03 21:22:32 +0200

    Add new code to the settings.js

---
+++ dist/js/components/AmountWidget.js
@@ -0,0 +1,77 @@
+import { select, settings } from './settings.js';
+
+class AmountWidget {
+  constructor(element){
+    const thisWidget = this;
+
+    console.log('AmountWidget:', thisWidget);
+    console.log('constructor arguments:', element);
+
+    thisWidget.getElements(element);
+    thisWidget.setValue(thisWidget.input.value || settings.AmountWidget.defaultValue);
+    thisWidget.initActions();
+    thisWidget.announce();
+  }
+
+  getElements(element){
+    const thisWidget = this;
+
+    thisWidget.element = element;
+    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+  }
+
+  setValue(value){
+    const thisWidget = this;
+
+    const newValue = parseInt(value);
+
+    /* TODO: Add validation */
+
+    /* check if the specified value(newValue) is different from that in the thisWidget.value and if is a number (NOT NULL) */
+    if(thisWidget.value !== newValue && !isNaN(newValue)){
+
+      /* check if the value is between 1-9 */
+      if(newValue >= settings.AmountWidget.defaultMin && newValue <= settings.AmountWidget.defaultMax){
+        /* yes, it's between, so make this */
+        thisWidget.value = newValue;
+      }
+    }
+    thisWidget.input.value = thisWidget.value;
+    thisWidget.announce();
+  }
+
+  initActions(){
+    const thisWidget = this;
+    console.log(thisWidget.initActions);
+
+    /* add 'change' as event listener and use the method setValue for thisWidget.input */
+    thisWidget.input.addEventListener('change', function(){
+      thisWidget.setValue(thisWidget.input.value);
+    });
+
+    /* add 'click' as event listener and use the method setValue for thisWidget.linkDecrease */
+    thisWidget.linkDecrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value - 1);
+    });
+
+    /* add 'click' as event listener and use the method setValue for thisWidget.linkIncrease */
+    thisWidget.linkIncrease.addEventListener('click', function(event){
+      event.preventDefault();
+      thisWidget.setValue(thisWidget.value + 1);
+    });
+  }
+
+  announce(){
+    const thisWidget = this;
+
+    const event = new CustomEvent('updated',{
+      bubbles: true
+    });
+    thisWidget.element.dispatchEvent(event);
+  }
+}
+
+export default AmountWidget;
---
+++ dist/js/components/CartProduct.js
@@ -0,0 +1,98 @@
+import { select } from './settings.js';
+import AmountWidget from './components/AmountWidget.js';
+
+class CartProduct {
+  constructor(menuProduct, element){
+    const thisCartProduct = this;
+    console.log('cartProduct:', thisCartProduct);
+
+    thisCartProduct.id = menuProduct.id;
+    thisCartProduct.name = menuProduct.name;
+    thisCartProduct.amount = menuProduct.amount;
+    thisCartProduct.price = menuProduct.price;
+    thisCartProduct.priceSingle = menuProduct.priceSingle;
+    thisCartProduct.params = menuProduct.params;
+
+    thisCartProduct.getElements(element);
+    thisCartProduct.initAmountWidget();
+    thisCartProduct.initActions();
+    
+  }
+
+  getElements(element){
+    const thisCartProduct = this;
+
+    thisCartProduct.dom = {};
+    thisCartProduct.dom.wrapper = element;
+
+    thisCartProduct.dom.AmountWidgetElem = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.AmountWidget);
+    console.log(thisCartProduct.dom.AmountWidgetElem);
+
+    thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+    console.log(thisCartProduct.dom.price);
+
+    thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+    console.log(thisCartProduct.dom.edit);
+
+    thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    console.log(thisCartProduct.dom.remove);
+  }
+
+  initAmountWidget(){
+    const thisCartProduct = this;
+
+    thisCartProduct.AmountWidget = new AmountWidget(thisCartProduct.dom.AmountWidgetElem);
+
+    thisCartProduct.dom.AmountWidgetElem.addEventListener('updated', function(){
+      console.log('price', thisCartProduct.price);
+      thisCartProduct.price = thisCartProduct.AmountWidget.value * thisCartProduct.priceSingle;
+      thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+    });
+  }
+
+  remove(){
+    const thisCartProduct = this;
+    console.log(thisCartProduct.remove);
+
+    const event = new CustomEvent('remove', {
+      bubbles: true,
+      detail: {
+        cartProduct: thisCartProduct,
+      },
+    });
+
+    thisCartProduct.dom.wrapper.dispatchEvent(event);
+  }
+
+  initActions(){
+    const thisCartProduct = this;
+
+    thisCartProduct.dom.edit.addEventListener('click', function(event){
+      event.preventDefault();
+    });
+
+    thisCartProduct.dom.remove.addEventListener('click', function(event){
+      event.preventDefault();
+      thisCartProduct.remove();
+    });
+  }
+
+  getData(){
+    const thisCartProduct = this;
+    console.log(thisCartProduct.getData);
+
+    const productCartSummary = {
+      id: thisCartProduct.id,
+      name: thisCartProduct.name,
+      amount: thisCartProduct.amount,
+      price: thisCartProduct.price,
+      priceSingle: thisCartProduct.priceSingle,
+      params: thisCartProduct.params,
+    };
+    console.log(productCartSummary);
+
+    return productCartSummary;
+  }
+}
+
+export default CartProduct;
