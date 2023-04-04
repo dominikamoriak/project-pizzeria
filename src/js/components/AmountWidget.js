9270e1d1c02fc741b9de44917c98f930d12dc9ad
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-04 21:23:40 +0200

    Create new class BaseWidget and edit class AmountWidget

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,77 +1,60 @@
 import { select, settings } from '../settings.js';
+import BaseWidget from '../componentsBaseWidget.js';
 
-class AmountWidget {
+class AmountWidget extends BaseWidget{
   constructor(element){
+    super(element, settings.AmountWidget.defaultValue);
+
     const thisWidget = this;
 
-    console.log('AmountWidget:', thisWidget);
-    console.log('constructor arguments:', element);
+    //console.log('AmountWidget:', thisWidget);
+    //console.log('constructor arguments:', element);
 
     thisWidget.getElements(element);
-    thisWidget.setValue(thisWidget.input.value || settings.AmountWidget.defaultValue);
     thisWidget.initActions();
     thisWidget.announce();
   }
 
-  getElements(element){
+  getElements(){
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-  
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value){
+  isValid(value){
+    return !isNaN(value)
+    && value >= settings.AmountWidget.defaultMin 
+    && value <= settings.AmountWidget.defaultMax;
+  }
+
+  renderValue(){
     const thisWidget = this;
 
-    const newValue = parseInt(value);
-
-    /* TODO: Add validation */
-
-    /* check if the specified value(newValue) is different from that in the thisWidget.value and if is a number (NOT NULL) */
-    if(thisWidget.value !== newValue && !isNaN(newValue)){
-
-      /* check if the value is between 1-9 */
-      if(newValue >= settings.AmountWidget.defaultMin && newValue <= settings.AmountWidget.defaultMax){
-        /* yes, it's between, so make this */
-        thisWidget.value = newValue;
-      }
-    }
-    thisWidget.input.value = thisWidget.value;
-    thisWidget.announce();
+    thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions(){
     const thisWidget = this;
     console.log(thisWidget.initActions);
 
-    /* add 'change' as event listener and use the method setValue for thisWidget.input */
-    thisWidget.input.addEventListener('change', function(){
-      thisWidget.setValue(thisWidget.input.value);
+    /* add 'change' as event listener and use the method setValue for thisWidget.dom.input */
+    thisWidget.dom.input.addEventListener('change', function(){
+      thisWidget.setValue(thisWidget.dom.input.value);
     });
 
-    /* add 'click' as event listener and use the method setValue for thisWidget.linkDecrease */
-    thisWidget.linkDecrease.addEventListener('click', function(event){
+    /* add 'click' as event listener and use the method setValue forthisWidget.dom.linkDecrease */
+    thisWidget.dom.linkDecrease.addEventListener('click', function(event){
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
     });
 
-    /* add 'click' as event listener and use the method setValue for thisWidget.linkIncrease */
-    thisWidget.linkIncrease.addEventListener('click', function(event){
+    /* add 'click' as event listener and use the method setValue for thisWidget.dom.linkIncrease */
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event){
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
-  }
-
-  announce(){
-    const thisWidget = this;
-
-    const event = new CustomEvent('updated',{
-      bubbles: true
-    });
-    thisWidget.element.dispatchEvent(event);
   }
 }
 
