970a1ce4781bace2fe965c44c9b6c9137e596db7
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-04 21:24:31 +0200

    Edit

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,5 +1,5 @@
 import { select, settings } from '../settings.js';
-import BaseWidget from '../componentsBaseWidget.js';
+import BaseWidget from '../components/BaseWidget.js';
 
 class AmountWidget extends BaseWidget{
   constructor(element){
