d9277d9887b074e6c029bd2c5516131d5ea547bd
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-07 20:48:28 +0200

    Edit

--- src/js/components/booking.js
+++ src/js/components/booking.js
@@ -169,16 +169,16 @@ class booking{
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper),
     thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper),
     
-    thisBooking.dom.wrapper.innerHTML = generatedHTML;
-
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
  
     thisBooking.dom.tables = thisBooking.dom.wrapper.querySelector(select.booking.tables);
   
-    thisBooking.dom.bookingForm = thisBooking.dom.wrapper.querySelector(select.containerOf.booking);
-    thisBooking.dom.address = thisBooking.dom.wrapper.querySelector(select.booking.address);
-    thisBooking.dom.phone = thisBooking.dom.wrapper.querySelector(select.booking.phone);
+    thisBooking.dom.bookingForm = thisBooking.dom.wrapper.querySelector('.booking-form');
+    thisBooking.dom.address = thisBooking.dom.wrapper.querySelector('.booking-address');
+    thisBooking.dom.phone = thisBooking.dom.wrapper.querySelector('.booking-phone');
+  
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
   }
 
   initWidgets(){
