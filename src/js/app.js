7be9192698c9bd081a21f0e24cad7cf73000ed7b
Dominika Moriak <dominika.moriak@gmail.com>
2023-04-12 13:31:55 +0200

    Edit

--- src/js/app.js
+++ src/js/app.js
@@ -100,7 +100,7 @@ const app = {
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
     thisApp.productList.addEventListener('add-to-cart', function(event){
-      app.cart.add(event.detail.product.preparecartproduct());
+      app.cart.add(event.detail.product.prepareCartProduct());
     });
   },
 
